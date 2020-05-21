0e5c9d1bafbac3bfd0b1e7090e8408a2c1b18a2e
Yuliia <julia.gancheva1990@gmail.com>
2020-05-21 16:51:26 +0300

    Master - Loading data from profile created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {makeApiClient} from './lib/api'
+
+let apiClient = makeApiClient('https://api.github.com')
 
 function App() {
+  let [user, setUser] = useState(null)
+  let [loading, setLoading] = useState(true)
+  let [error, setError] = useState(null)
+
+  useEffect(() => {
+    apiClient.fetchJSON('/users/juliatalashova').then(user =>{
+      setUser(user)
+      setLoading(false)
+    })
+      .catch(error => setError(error))
+  },[])
+
+  if(error) {
+    return <Error error={error} />
+  }
+
+  if(loading) {
+    return <Loading />
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>GitHub profile</h3>
+      <img width="150px" src={user.avatar_url} alt="avatar"/>
+      <h5>{user.name}</h5>
+      <div>{user.bio}</div>
+      <pre>
+        <code>{JSON.stringify(user, null, 2)}</code>
+      </pre>
     </div>
   );
 }
-
 export default App;
+
+function Loading() {
+    return <div>
+      Loading...
+    </div>
+}
+function Error(error) {
+  return <div>{String(error)}</div>
+}
---
+++ src/lib/api.js
@@ -0,0 +1,31 @@
+import fetch from 'cross-fetch'
+import * as R from 'ramda'
+
+export function makeApiClient(apiOrigin) {
+  async function fetchJSON(url, options = {}) {
+    if(!url.startsWith("http")) {
+      url = apiOrigin + url
+    }
+    options = R.mergeDeepRight(options, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(options.body)
+    })
+
+    let response = await fetch(url, options)
+    if((response.headers.get('content-type') || '').includes('application/json')) {
+      try {
+        return response.json()
+      } catch (error) {
+        throw new Error('API: Invalid JSON')
+      }
+    } else {
+      throw new Error ('API: Invalid mime-type')
+    }
+  }
+  return {
+    fetchJSON
+  }
+}
